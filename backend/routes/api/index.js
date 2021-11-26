5cf15164798c9cbe0fcc72982911fdac6758e0a2
Justin Russo <j.russo@outlook.com>
2021-11-26 16:29:40 -0500

    feat(backend): implement auth

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const asyncHandler = require("express-async-handler");
+
+const { setTokenCookie } = require("../../utils/auth.js");
+const { User } = require("../../db/models");
 
 const router = express.Router();
 
