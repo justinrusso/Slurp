282ca586fbe7d3d3debf3d949102f379673c0b87
Justin Russo <j.russo@outlook.com>
2021-11-26 16:32:47 -0500

    feat(backend): login route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
 
+const { sessionRouter } = require("./session");
 const { setTokenCookie } = require("../../utils/auth.js");
 const { User } = require("../../db/models");
+const { usersRouter } = require("./users");
 
 const router = express.Router();
+
+router.use("/session", sessionRouter);
+router.use("/users", usersRouter);
 
 module.exports = {
   apiRouter: router,
