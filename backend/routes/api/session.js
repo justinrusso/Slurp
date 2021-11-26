415d746e4390dab531af7cff4630c8d4b66efd90
Justin Russo <j.russo@outlook.com>
2021-11-26 16:38:24 -0500

    feat(backend): validation login

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
+const { check } = require("express-validator");
 
+const { handleValidationErrors } = require("../../utils/validation");
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
+
+const validateLogin = [
+  check("credential")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email or username."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+  handleValidationErrors,
+];
 
 const router = express.Router();
 
@@ -20,6 +33,7 @@ router.get("/", restoreUser, (req, res) => {
 // Log in
 router.post(
   "/",
+  validateLogin,
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
