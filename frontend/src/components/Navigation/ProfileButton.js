71047a587ed7d4265c264e02642d1d5a4e9e88f1
Justin Russo <j.russo@outlook.com>
2021-12-02 15:12:17 -0500

    feat(frontend): implement profile menu

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,8 +1,55 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
 
-const ProfileButton = () => {
-  return <FontAwesomeIcon icon={faUserCircle} />;
+import { logout } from "../../store/session";
+
+const ProfileButton = ({ user }) => {
+  const dispatch = useDispatch();
+
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const openMenu = () => {
+    if (menuVisible) {
+      return;
+    }
+    setMenuVisible(true);
+  };
+
+  useEffect(() => {
+    if (!menuVisible) {
+      return;
+    }
+
+    const closeMenu = () => setMenuVisible(false);
+
+    document.addEventListener("click", closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  });
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(logout());
+  };
+
+  return (
+    <>
+      <button onClick={openMenu}>
+        <FontAwesomeIcon icon={faUserCircle} />
+      </button>
+      {menuVisible && (
+        <ul className="profile-dropdown">
+          <li>{user.username}</li>
+          <li>{user.email}</li>
+          <li>
+            <button onClick={handleLogout}>Log Out</button>
+          </li>
+        </ul>
+      )}
+    </>
+  );
 };
 
 export default ProfileButton;
