7144b31ce02ac429d1206ac5c5913b827a8b8add
Justin Russo <j.russo@outlook.com>
2021-12-06 12:26:58 -0500

    refactor(frontend): simplify Base css generation

--- frontend/src/components/styled/Base.js
+++ frontend/src/components/styled/Base.js
@@ -1,52 +1,8 @@
 import styled from "styled-components";
-import { camelToKebabCase } from "../../utils";
 
-/**
- *
- * @param {{[key: string]: any}} prop
- * @returns {string}
- */
-const cssPropToCss = (prop) => {
-  return Object.entries(prop)
-    .map(([cssKey, cssVal]) => `${camelToKebabCase(cssKey)}: ${cssVal};`)
-    .join("");
-};
-
-/**
- *
- * @param {{[key: string]: any}} prop
- * @returns {string}
- */
-const selectorsPropToCss = (prop) => {
-  return Object.entries(prop)
-    .map(
-      ([selector, selectorProps]) =>
-        `${selector} {${cssPropToCss(selectorProps)}}`
-    )
-    .join("");
-};
-
-const acceptedProps = new Map([
-  ["css", cssPropToCss],
-  ["cssSelectors", selectorsPropToCss],
-]);
-
-/**
- *
- * @param {[propKey, propValue]} propPair
- * @returns {string}
- */
-const rootPropToCss = ([propKey, prop]) => {
-  return acceptedProps.get(propKey)(prop);
-};
-
-const filterAcceptedProps = ([propKey]) => acceptedProps.has(propKey);
-
-const propsToCss = (props) =>
-  Object.entries(props).filter(filterAcceptedProps).map(rootPropToCss);
-
-const Base = styled.div`
-  ${(props) => propsToCss(props)}
-`;
+const Base = styled.div((props) => ({
+  ...props.css,
+  ...props.cssSelectors,
+}));
 
 export default Base;
