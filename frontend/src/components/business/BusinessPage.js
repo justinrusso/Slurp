abcc94881b0ffa6bd08d54885b5081ec55713b54
Justin Russo <j.russo@outlook.com>
2021-12-06 23:46:48 -0500

    feat(frontend): main content layout for biz page

--- frontend/src/components/business/BusinessPage.js
+++ frontend/src/components/business/BusinessPage.js
@@ -3,11 +3,61 @@ import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { selectBusiness } from "../../store/businesses";
 import ErrorPage from "../common/ErrorPage";
+import Container from "../styled/Container";
 
 const PhotoHeader = styled.div`
-  position: relative;
+  background-color: #333;
+  color: #fff;
   height: 426px;
+  position: relative;
   width: 100%;
+`;
+
+const PhotoHeaderContentContainer = styled.div`
+  display: flex;
+  align-items: flex-end;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  padding: ${(props) => props.theme.spacing.gen(5, 8)};
+  position: absolute;
+  right: 0;
+  top: 0;
+`;
+
+const PhotoHeaderContent = styled.div`
+  flex: 1 auto;
+  pointer-events: none;
+  position: relative;
+  z-index: 1;
+  padding-right: ${(props) => props.theme.spacing.gen(2)};
+`;
+
+const MainContainer = styled(Container).attrs(() => ({ as: "main" }))`
+  display: flex;
+  margin-top: ${(props) => props.theme.spacing.gen(3)};
+`;
+
+const MainLeft = styled.div`
+  width: 100%;
+
+  ${(props) => props.theme.breakpoints.up("lg")} {
+    width: ${(2 / 3) * 100}%;
+  }
+`;
+
+const MainRight = styled.div`
+  display: none;
+  width: ${(1 / 3) * 100}%;
+
+  ${(props) => props.theme.breakpoints.up("lg")} {
+    display: block;
+  }
+`;
+
+const Sidebar = styled.div`
+  position: sticky;
+  top: 18px;
 `;
 
 const BusinessPage = () => {
@@ -15,7 +65,25 @@ const BusinessPage = () => {
 
   const business = useSelector(selectBusiness(businessId));
 
-  return business ? <PhotoHeader></PhotoHeader> : <ErrorPage />;
+  return business ? (
+    <>
+      <PhotoHeader>
+        <PhotoHeaderContentContainer>
+          <PhotoHeaderContent>
+            <h1>{business.name}</h1>
+          </PhotoHeaderContent>
+        </PhotoHeaderContentContainer>
+      </PhotoHeader>
+      <MainContainer>
+        <MainLeft></MainLeft>
+        <MainRight>
+          <Sidebar></Sidebar>
+        </MainRight>
+      </MainContainer>
+    </>
+  ) : (
+    <ErrorPage />
+  );
 };
 
 export default BusinessPage;
