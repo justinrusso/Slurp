780399ef92673dcec2dd3c994b0a550f32b620e7
Justin Russo <j.russo@outlook.com>
2021-12-08 22:01:06 -0500

    fix(frontend): path param error in biz create form

--- frontend/src/components/business/BusinessPages.js
+++ frontend/src/components/business/BusinessPages.js
@@ -15,11 +15,10 @@ export const businessRouteRoot = "/biz";
 const makeRoute = (subRoute) => `${businessRouteRoot}${subRoute}`;
 
 const BusinessPages = () => {
-  const {
-    params: { businessId },
-  } = useRouteMatch({
+  const match = useRouteMatch({
     path: makeRoute("/:businessId(\\d+)"),
   });
+  const businessId = match?.params?.businessId;
 
   const business = useSelector(selectBusiness(businessId));
 
